d9e94dc2c8404de95440dc6311e9771921962b5c
Пользователь <gorobnuak.l@gmail.com>
2021-04-10 15:33:52 +0300

    Добавьте файлы проекта.

---
+++ HW2_1_/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace HW2_1
+{
+    namespace HW2._1
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+                Console.WriteLine("Введіть літеру латинського алфавіту");
+                char ch = (char)Console.Read();
+                char ch2 = ' ';
+                if (ch > 'a' && ch < 'z')
+                {
+                    ch2 = char.ToUpper(ch);
+                    Console.WriteLine($"{ch} нижнього регістру = {ch2} верхнього регістру");
+                }
+                else if (ch > 'A' && ch < 'Z')
+                {
+                    ch2 = char.ToLower(ch);
+                    Console.WriteLine($"{ch} верхнього регістру = {ch2} нижнього регістру");
+                }
+                else
+                {
+                    Console.WriteLine("Не вірно введені дані");
+                }
+            }
+        }
+    }
+}
